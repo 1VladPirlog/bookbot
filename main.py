845c0baf2fc4d54ff43c3cb303b4e88fce4e4dba
1VladPirlog <vladpirlog@ymail.com>
2025-04-17 13:55:19 +0300

    no function to count char as dictionary unsorted

--- main.py
+++ main.py
@@ -5,13 +5,20 @@ def get_book_text(filepath):
         return f.read()
 
 from stats import get_word_count
+from stats import get_char_count
 
 def main():
     filename = "books/frankenstein.txt"
     content = get_book_text(filename)
+    
     if content is not None:
         word_count = get_word_count(content)
         print(f"{word_count} words found in the document")
+        
+    if content:
+        char_count = get_char_count(content)
+        for char, count in char_count.items():
+            print(f"{char_count}")
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -3,3 +3,14 @@ def get_word_count(text):
         return 0
     words = text.split()
     return len(words)
+
+def get_char_count(text):
+    
+    content_lower = text.lower()
+    char_dict = {}
+    for char in content_lower:
+        if char in char_dict:
+            char_dict[char] += 1
+        else:
+            char_dict[char] = 1
+    return char_dict
